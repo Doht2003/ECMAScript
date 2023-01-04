63579fb5550ade42fe7cf4f6c5fb03fde9ab24f4
Doht2003 <bangngoc535@gmail.com>
2023-01-04 11:49:06 +0700

    day01

--- chap01/main.js
+++ chap01/main.js
@@ -1,17 +1,73 @@
-document.addEventListener('DOMContentLoaded', function(){
-    function $(selector){
-        // checking
-        if(typeof selector !== "string") return "";
-        // processing
-        const selectors = document.querySelectorAll(selector);
-        //return 
-        return selectors.length == 1 ? selectors[0] : selectors;
+// var - let - const
+var a = 10;
+var a = 20;
+
+console.log(a);
+
+// ES6
+let b = 10;
+b = 20;
+
+const names = ["Kien", "Dat", "Lam"];
+const person = {
+    name: "Dat",
+};
+person.age = 20;
+
+console.log(person);
+
+// names.push("Do");
+// console.log(names);
+
+// const nameElement = document.querySelector("#nameElement");
+
+// let result = "";
+
+// for (let i = 0; i < names.length; i++) {
+//     result += "<div>" + names[i] + "</div>";
+// }
+// nameElement.innerHTML = result;
+
+// function name
+console.log("function name", sum(30, 10));
+function sum(a, b) {
+    return a + b;
+}
+
+// function expression -> anonymous function
+const sum2 = function (a, b) {
+    return a + b;
+};
+console.log("function expression", sum2(30, 10));
+// arrow function - Viết tắt của function expression
+const sum3 = (a, b) => a + b;
+console.log("arrow function ", sum3(30, 40));
+// IIFE function
+((a, b) => {
+    console.log("IIFE function", a + b);
+})(50, 50);
+
+function total(a, b) {
+    // checking
+    if (typeof a == "string" || typeof b == "string") return 0;
+    // processing
+    const result = a + b;
+    // return
+    return result;
+}
+console.log(total(10, 20));
+
+const nameElement = document.querySelector("#nameElement");
+const productList = ["product A", "product B"];
+function showProduct(products) {
+    // checking
+    if (!Array.isArray(products)) return [];
+    // processing
+    let result = "";
+    for (let i = 0; i < products.length; i++) {
+        result += "<div>" + products[i] + "</div>";
     }
-    
-    const menuItem = $('.menu-item');
-    if(menuItem){
-        for(let i = 0 ; i < menuItem.length; i++){
-            menuItem[i].style.color = "red";
-        }
-    }
-})
+    // return
+    return result;
+}
+nameElement.innerHTML = showProduct(10);
